8218ba9290dc73455b9783a937b6e3730b265e8d
Johannes Schickling <schickling.j@gmail.com>
2016-08-30 14:41:36 +0200

    new prep

---
+++ prep.ts
@@ -0,0 +1,11 @@
+import './src/polyfill'
+import { chapters } from './src/utils/content'
+
+export default () => {
+  const chapterRoutes = chapters
+    .reduce((acc, c) => acc.concat(c.subchapters.map((s) => `/${c.alias}/${s.alias}`)), [])
+
+  return {
+    routes: chapterRoutes.concat(['/']),
+  }
+}
