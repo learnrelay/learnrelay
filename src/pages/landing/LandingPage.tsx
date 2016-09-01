a3afd3574096dbc3b1b58e4246ebbd5fe69e5d6d
Nilan <nilan.marktanner@student.kit.edu>
2016-09-01 22:36:16 +0200

    prepares landing page

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -26,11 +26,23 @@ export default class LandingPage extends React.Component<Props, {}> {
             <p>You have heard about Relay.js for a while but you are not quite sure what it really is, how it is useful, and how to start your first Relay application. If you have these questions in your mind, you come to the right place!</p>
           </dt>
           <dt>
-            <h3>Overview</h3>
+            <h3>Queries</h3>
             <p>You have heard about Relay.js for a while but you are not quite sure what it really is, how it is useful, and how to start your first Relay application. If you have these questions in your mind, you come to the right place!</p>
           </dt>
           <dt>
-            <h3>Introduction to Relay</h3>
+            <h3>Connections</h3>
+            <p>You have heard about Relay.js for a while but you are not quite sure what it really is, how it is useful, and how to start your first Relay application. If you have these questions in your mind, you come to the right place!</p>
+          </dt>
+          <dt>
+            <h3>Routes</h3>
+            <p>You have heard about Relay.js for a while but you are not quite sure what it really is, how it is useful, and how to start your first Relay application. If you have these questions in your mind, you come to the right place!</p>
+          </dt>
+          <dt>
+            <h3>Mutations</h3>
+            <p>You have heard about Relay.js for a while but you are not quite sure what it really is, how it is useful, and how to start your first Relay application. If you have these questions in your mind, you come to the right place!</p>
+          </dt>
+          <dt>
+            <h3>Go further</h3>
             <p>You have heard about Relay.js for a while but you are not quite sure what it really is, how it is useful, and how to start your first Relay application. If you have these questions in your mind, you come to the right place!</p>
           </dt>
         </dl>
